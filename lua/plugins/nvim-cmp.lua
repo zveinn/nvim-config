e341136eea6efb79fb79c932e3c8b1a720e04dd8
zveinn <info@sveinn.is>
2023-11-08 11:25:38 +0000

    allowing parallel golang ci runners

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -183,7 +183,7 @@ if not configs.golangcilsp then
 			root_dir = lspconfig.util.root_pattern('.git', 'go.mod'),
 			init_options = {
 				-- command = { "golangci-lint", "run", "--enable-all", "--disable", "lll", "--out-format", "json", "--issues-exit-code=1" };
-				command = { 'golangci-lint', 'run', '--out-format', 'json' },
+				command = { 'golangci-lint', 'run', '--out-format', 'json', '--allow-parallel-runners' },
 			}
 		},
 	}
